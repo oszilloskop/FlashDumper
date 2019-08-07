7d0045317ff0de80c36ca900177c775a728093c5
JuS <sagurna@s3i.de>
2019-08-07 17:26:11 +0200

    FLASHROM_PROGRAMMER_COMMAND heist jetzt FLASHROM_PROGRAMMER_PARAMETER

--- flashdumper.sh
+++ flashdumper.sh
@@ -13,7 +13,7 @@ histchars=
 #-------------------------------------------------------------------------------
 # "flashrom"-Tool Parameter
 #-------------------------------------------------------------------------------
-FLASHROM_PROGRAMMER_COMMAND="linux_spi:dev=/dev/spidev0.0,spispeed=1000"
+FLASHROM_PROGRAMMER_PARAMETER="linux_spi:dev=/dev/spidev0.0,spispeed=1000"
 SUDO_CMD="sudo -S"
 
 #-------------------------------------------------------------------------------
@@ -312,7 +312,7 @@ else
          echo
          echo Bitte mehrere Minuten warten...
          echo
-         echo "$PASSWORD" | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_COMMAND" -w $ABBILD
+         echo "$PASSWORD" | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_PARAMETER" -w $ABBILD
          echo
          echo Abarbeitung abgeschlossen.
          echo Bitte obige Text-Ausgabe überprüfen!
@@ -370,11 +370,11 @@ dialog --title "$TITEL" \
        echo Bitte mehrere Minuten warten...
        echo
        echo Lesen:
-       echo "$PASSWORD" | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_COMMAND" -r $DUMPFILENAME
+       echo "$PASSWORD" | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_PARAMETER" -r $DUMPFILENAME
        echo "$PASSWORD" | $SUDO_CMD chown $USER: "$DUMPFILENAME"
        echo
        echo Verify:
-       echo "$PASSWORD" | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_COMMAND" -v $DUMPFILENAME
+       echo "$PASSWORD" | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_PARAMETER" -v $DUMPFILENAME
        echo
        echo Die Dump-Datei wurde abgespeichert als
        echo ./$FLASHDUMPDIRECTORY/$ROUTERFOLDER/$DUMPFILENAME
@@ -472,7 +472,7 @@ fi
          echo
          echo Bitte mehrere Minuten warten...
          echo
-         echo "$PASSWORD" | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_COMMAND" -w $INFILE
+         echo "$PASSWORD" | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_PARAMETER" -w $INFILE
          echo
          echo Abarbeitung abgeschlossen.
          echo Bitte obige Text-Ausgabe überprüfen!
@@ -519,7 +519,7 @@ dialog --title "$TITEL" \
        --prgbox "
        echo Bitte warten...
        echo
-       echo $PASSWORD | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_COMMAND"
+       echo $PASSWORD | $SUDO_CMD flashrom -p "$FLASHROM_PROGRAMMER_PARAMETER"
        echo
        echo Abarbeitung abgeschlossen.
        echo Bitte obige Text-Ausgabe überprüfen!
